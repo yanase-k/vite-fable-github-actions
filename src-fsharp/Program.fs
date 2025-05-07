39b0d4110af207dfb1a5367e46813729fda8c129
yanase-k <k.yanase@m-aifroide.com>
2025-05-07 16:10:26 +0900

    Update Program.fs

--- src-fsharp/Program.fs
+++ src-fsharp/Program.fs
@@ -1,2 +1,7 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+module Program
+
+open Browser.Dom
+
+let element = document.getElementById "app"
+element.innerText <- "Vite + Fable + GitHub Actions テスト"
